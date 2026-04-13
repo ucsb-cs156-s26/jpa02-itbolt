8fce2d1636bba3dcb332f1c51fe13520b3765672
Tien Minh Nguyen <tienminh@ucsb.edu>
2026-04-13 11:50:14 -0700

    tm - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -26,6 +26,10 @@ public class DeveloperTest {
         // <https://bit.ly/cs156-s26-teams>
         assertEquals("Tien Minh Nguyen", Developer.getName());
     }
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("itbolt", Developer.getGithubId());
+    }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
